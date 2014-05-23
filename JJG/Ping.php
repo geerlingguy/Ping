41cebcf4cfe9f8fd21ec3229ffa81722476105d5
RJ <vpsmt.com@gmail.com>
2014-05-23 15:52:16 -0400

    Revert "changes"
    This reverts commit 86c85777fc1a1ba1ead4d9b6e35a773890e5883d.

--- JJG/Ping.php
+++ JJG/Ping.php
@@ -19,22 +19,16 @@
  *
  * @version 1.0.0
  * @author Jeff Geerling.
- * modified by vpsmt.com
  */
 
 namespace JJG;
 
 class Ping {
 
-	private $host;
-	private $ttl;
-	private $port = 80;
-	private $ping_count = 1;
-	private $max_ping_count = 5;
-	private $data = 'Ping';
-	private $host_type = '';
-  	private $timeout = 5; //in sec
-
+  private $host;
+  private $ttl;
+  private $port = 80;
+  private $data = 'Ping';
 
   /**
    * Called when the Ping object is created.
@@ -51,17 +45,17 @@ class Ping {
    *     - 64 = same region
    *     - 128 = same continent
    *     - 255 = unrestricted
-   *   
+   *   The TTL is also used as a general 'timeout' value for fsockopen(), so if
+   *   you are using that method, you might want to set a default of 5-10 sec to
+   *   avoid blocking network connections.
    */
-  public function __construct($host = '', $ttl = 255) {
+  public function __construct($host, $ttl = 255) {
+    if (!isset($host)) {
+      throw new Exception("Error: Host name not supplied.");
+    }
 
-  	$this->host = $host;
-  	$this->ttl = $ttl;
-  	$this->host_type = 'unix';
-  	if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')
-  		$this->host_type = 'windows';
-  	else if (strtoupper(PHP_OS) === 'DARWIN')
-  		$this->host_type = 'mac';
+    $this->host = $host;
+    $this->ttl = $ttl;
   }
 
   /**
@@ -71,18 +65,8 @@ class Ping {
    *   TTL in hops.
    */
   public function setTtl($ttl) {
-  	$this->ttl = $ttl;
+    $this->ttl = $ttl;
   }
-
-    /**
-   * Set the timeout value
-   *
-   * @param int $timeout
-   * in sec
-   */
-    public function setTimeout($timeout) {
-    	$this->timeout = intval($timeout);
-    }
 
   /**
    * Set the host.
@@ -91,7 +75,7 @@ class Ping {
    *   Host name or IP address.
    */
   public function setHost($host) {
-  	$this->host = $host;
+    $this->host = $host;
   }
 
   /**
@@ -105,19 +89,7 @@ class Ping {
    *   Port to use for fsockopen ping (defaults to 80 if not set).
    */
   public function setPort($port) {
-  	$this->port = $port;
-  }
-
-  /**
-   * Set the ping count
-   *
-   * @param int $count
-   */
-  public function setPingCount($count) {
-  	$count = intval($count);
-  	if ($count < 1) $count = 1;
-  	if ($count > $this->max_ping_count) $count = $this->max_ping_count;
-  	$this->ping_count = $count;
+    $this->port = $port;
   }
 
   /**
@@ -132,95 +104,72 @@ class Ping {
    *       environments, as creating a SOCK_RAW socket requires root privileges.
    *
    * @return mixed
-   * return latency in milliseconds, return false if offline
+   *   Latency as integer, in ms, if host is reachable or FALSE if host is down.
    */
   public function ping($method = 'exec') {
+    $latency = false;
 
-  	$latency = false;
-
-  	switch ($method) {
+    switch ($method) {
       // The exec method uses the possibly insecure exec() function, which
       // passes the input to the system. This is potentially VERY dangerous if
       // you pass in any user-submitted data. Be SURE you sanitize your inputs!
-  		case 'exec':
-  		$ttl = escapeshellcmd($this->ttl);
-  		$host = escapeshellcmd($this->host);
+      case 'exec':
+        $ttl = escapeshellcmd($this->ttl);
+        $host = escapeshellcmd($this->host);
         // Exec string for Windows-based systems.
-
-  		if ($this->host_type == 'windows') {
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
           // -n = number of pings; -i = ttl.
-  			$milli_sec = $this->timeout * 1000;
-  			$timeout_string = '';
-  			if ($milli_sec > 0) $timeout_string = ' -w '.$milli_sec.' ';
-          $exec_string = 'ping -n '.$this->ping_count.' -i ' . $ttl . $timeout_string.' ' . $host;  //default time out is 4 seconds (ie. -w 4000)
-     }
-     else if ($this->host_type == 'mac') {
-          // -n = numeric output; -c = number of pings; -m = ttl; -t = timeout in sec
-     	$timeout_string = '';
-     	if ($this->timeout > 0) $timeout_string = ' -t '.$this->timeout.' ';
-     	$exec_string = 'ping -n -c '.$this->ping_count.' -m ' . $ttl . $timeout_string.' ' . $host;
-     }
-        // Exec string for UNIX-based systems (Linux).
-     else {
-          // -n = numeric output; -c = number of pings; -t = ttl; -w = deadline in sec
-     	$timeout_string = '';
-     	if ($this->timeout > 0) $timeout_string = ' -w '.$this->timeout.' ';
-     	$exec_string = 'ping -n -c '.$this->ping_count.' -t ' . $ttl . $timeout_string.' ' . $host;
-     }
-     $str = exec($exec_string, $output, $return);
-
-     print_r($output);
-
-     if (!empty($output)) {
-     	//round trip calculation (ie. avg, min, max stddev) not always return (ie. when timed out, so use latency line to get avg latency)
-     	$matches = array_filter($output, array($this, 'findLatencyLine'));
-
-     	print_r($matches);
-
-     	$total_latency = 0;
-     	if (!empty($matches)) {
-     		foreach ($matches as $k=>$latency_line) {
-
-     			$current_latency_line_array = explode(' ', $latency_line);
-     			foreach ($current_latency_line_array as $item) {
-     				$item = trim($item);
-     				$pos = stripos($item, 'time=');
-     				if ($pos !== false) {
-     					$item = str_replace('time=', '', $item);
-     					$item = str_replace('ms', '', $item);
-     					$total_latency += floatval($item);
-     					break;	//check next latency_line
-     				}
-     			}
-              }//foreach
-				
-			$latency = round($total_latency / count($matches));
-              
-          }//latency line found
-		else {
-			$latency = false;
-		}
-      }
-      else {
-      	$latency = false;
-      }
-
-      break;
+          $exec_string = 'ping -n 1 -i ' . $ttl . ' ' . $host;
+          $host_type = 'windows';
+          $result_line = 2;
+          $time_param = 4;
+        }
+        // Exec string for UNIX-based systems (Mac, Linux).
+        else {
+          // -n = numeric output; -c = number of pings; -t = ttl.
+          $exec_string = 'ping -n -c 1 -t ' . $ttl . ' ' . $host;
+          $host_type = 'unix';
+          $result_line = 1;
+          $time_param = 6;
+        }
+        $str = exec($exec_string, $output, $return);
+        // If the result line in the output is not empty, parse it.
+        if (!empty($output[$result_line])) {
+          $array = explode(' ', $output[$result_line]);
+          // If the time parameter is missing, the host is unreachable.
+          if (!isset($array[$time_param])) {
+            $latency = false;
+          }
+          else {
+            // Remove 'time=' from latency stat.
+            $latency = str_replace('time=', '', $array[$time_param]);
+            // If on a windows machine, also remove the 'ms'.
+            if ($host_type == 'windows') {
+              $latency = str_replace('ms', '', $latency);
+            }
+            // Convert latency to microseconds.
+            $latency = round($latency);
+          }
+        }
+        else {
+          $latency = false;
+        }
+        break;
 
       // The fsockopen method simply tries to reach the host on a port. This
       // method is often the fastest, but not necessarily the most reliable.
       // Even if a host doesn't respond, fsockopen may still make a connection.
       case 'fsockopen':
-      $start = microtime(true);
-      $fp = fsockopen($this->host, $this->port, $errno, $errstr, $this->timeout);
-      if (!$fp) {
-      	$latency = false;
-      }
-      else {
-      	$latency = microtime(true) - $start;
-      	$latency = round($latency * 1000);
-      }
-      break;
+        $start = microtime(true);
+        $fp = fsockopen($this->host, $this->port, $errno, $errstr, $this->ttl);
+        if (!$fp) {
+          $latency = false;
+        }
+        else {
+          $latency = microtime(true) - $start;
+          $latency = round($latency * 1000);
+        }
+        break;
 
       // The socket method uses raw network packet data to try sending an ICMP
       // ping packet to a server, then measures the response time. Using this
@@ -229,50 +178,46 @@ class Ping {
       // where the script is not being run as a web user.
       case 'socket':
         // Create a package.
-      $type = "\x08";
-      $code = "\x00";
-      $checksum = "\x00\x00";
-      $identifier = "\x00\x00";
-      $seq_number = "\x00\x00";
-      $package = $type . $code . $checksum . $identifier . $seq_number . $this->data;
+        $type = "\x08";
+        $code = "\x00";
+        $checksum = "\x00\x00";
+        $identifier = "\x00\x00";
+        $seq_number = "\x00\x00";
+        $package = $type . $code . $checksum . $identifier . $seq_number . $this->data;
 
         // Calculate the checksum.
-      $checksum = $this->calculateChecksum($package);
+        $checksum = $this->calculateChecksum($package);
 
         // Finalize the package.
-      $package = $type . $code . $checksum . $identifier . $seq_number . $this->data;
+        $package = $type . $code . $checksum . $identifier . $seq_number . $this->data;
 
         // Create a socket, connect to server, then read socket and calculate.
-      if ($socket = socket_create(AF_INET, SOCK_RAW, 1)) {
-      	socket_connect($socket, $this->host, null);
-      	$start = microtime(true);
+        if ($socket = socket_create(AF_INET, SOCK_RAW, 1)) {
+          socket_connect($socket, $this->host, null);
+          $start = microtime(true);
           // Send the package.
-      	socket_send($socket, $package, strlen($package), 0);
-      	if (socket_read($socket, 255) !== false) {
-      		$latency = microtime(true) - $start;
-      		$latency = round($latency * 1000);
-      	}
-      	else {
-      		$latency = false;
-      	}
+          socket_send($socket, $package, strlen($package), 0);
+          if (socket_read($socket, 255) !== false) {
+            $latency = microtime(true) - $start;
+            $latency = round($latency * 1000);
+          }
+          else {
+            $latency = false;
+          }
           // Close the socket.
-      	socket_close($socket);
-      }
-      else {
-      	$latency = false;
-      }
+          socket_close($socket);
+        }
+        else {
+          $latency = false;
+        }
         // Close the socket.
-      socket_close($socket);
-      break;
- }
+        socket_close($socket);
+        break;
+    }
 
- return $latency;
-}
-
-private function findLatencyLine($haystack) {
-	$needle = 'time=';
-	return(stripos($haystack, $needle));
-}
+    // Return the latency.
+    return $latency;
+  }
 
   /**
    * Calculate a checksum.
@@ -284,17 +229,17 @@ private function findLatencyLine($haystack) {
    *   Binary string checksum of $data.
    */
   private function calculateChecksum($data) {
-  	if (strlen($data) % 2) {
-  		$data .= "\x00";
-  	}
+    if (strlen($data) % 2) {
+      $data .= "\x00";
+    }
 
-  	$bit = unpack('n*', $data);
-  	$sum = array_sum($bit);
+    $bit = unpack('n*', $data);
+    $sum = array_sum($bit);
 
-  	while ($sum >> 16) {
-  		$sum = ($sum >> 16) + ($sum & 0xffff);
-  	}
+    while ($sum >> 16) {
+      $sum = ($sum >> 16) + ($sum & 0xffff);
+    }
 
-  	return pack('n*', ~$sum);
+    return pack('n*', ~$sum);
   }
 }
