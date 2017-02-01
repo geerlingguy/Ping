dd14769475afbe912c5b3cdd896a8e5b693b093a
divinity76 <divinity76@gmail.com>
2017-02-01 21:55:07 +0100

    throw InvalidArgumentException in ping()

--- JJG/Ping.php
+++ JJG/Ping.php
@@ -194,6 +194,8 @@ class Ping {
       case 'socket':
         $latency = $this->pingSocket();
         break;
+      default:
+        throw new \InvalidArgumentException('unsupported ping method');
     }
 
     // Return the latency.
